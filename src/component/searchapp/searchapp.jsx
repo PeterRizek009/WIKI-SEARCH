b94acc435bd7cac20690d2134f1579919a00df60
Peter Shahin <petershahin00@gmail.com>
2023-02-22 21:42:25 +0200

    edit the sarch app

---
+++ src/component/searchapp/searchapp.jsx
@@ -0,0 +1,90 @@
+import React from 'react'
+import { useEffect, useState, useRef } from 'react'
+import axios from 'axios'
+import './searchapp.css'
+
+
+const SearchApp = ({ saved }) => {
+    const [term, setTerm] = useState(' ')
+    const [debounceSearch, setDebounceSearch] = useState(term)
+    const [result, setResult] = useState([])
+
+    const [bookmark, setBookmark] = useState(false);
+
+    console.log(saved);
+    const ref = useRef();
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            setDebounceSearch(term)
+            setBookmark(false);
+        }, 1200);
+        return () => clearTimeout(timeout)
+    }, [term])
+
+
+    useEffect(() => {
+        const search = async () => {
+            const responed = await axios.get('https://en.wikipedia.org/w/api.php', {
+                params: {
+                    action: 'query',
+                    list: 'search',
+                    origin: '*',
+                    format: 'json',
+                    srsearch: debounceSearch,
+                }
+            })
+            setResult(responed.data.query.search);
+
+        }
+        search()
+    }, [debounceSearch]);
+
+    const handleBookmark = () => {
+        setBookmark(!bookmark);
+        saved.push(term)
+    }
+
+    const fetchResult = result.map((el) => {
+        return (
+            <div className="card" key={el.pageid} ref={ref}>
+                <div className="card-title">
+                    <a href={`https://en.wikipedia.org/wiki/${el.title}`}>{el.title}</a>
+                </div>
+                <p><span dangerouslySetInnerHTML={{ __html: el.snippet }} /></p>
+            </div>
+        )
+    })
+
+    return (
+        <div className='container'>
+            <div className='search-bar'>
+                    <input
+                        type='text'
+                        className='search'
+                       
+                        onChange={(e) => setTerm(e.target.value)}
+                        value={term}/>
+                         <button type="button">  <i className="fas fa-search p-2"></i></button>
+                     
+               
+
+                {/* <div onClick={handleBookmark} className="mx-1 mt-4" >
+                    {bookmark === true ?
+                    <button type="button" class="btn btn-danger mx-2">Saved<i className="fas fa-star yellow"></i></button>
+                      :
+                      <button type="button" class="btn btn-outline-danger mx-2">Save Query <i className="fas fa-star"></i></button> }
+                </div> */}
+
+            </div>
+
+
+            <div className="card-container">
+                {fetchResult}
+            </div>
+        </div>
+
+
+    )
+}
+
+export default SearchApp
